8f6da3463a1b7f9b15e12d76cdbb8c3ce553f984
abhinandak1 <2243@tkmce.ac.in>
2023-12-17 22:30:48 +0530

    Initial commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,14 @@
+String mapBoxApiKey =
+    'pk.eyJ1IjoiYWJoaW5hbmQtYWsiLCJhIjoiY2xwcjR5MDNiMDFpeTJtcXB1eWVoejBwdSJ9.ZBLR4kdLEylmfFkWq52bXg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA0RDNmZTJDNUQyMGQxZjVkNDFCQUIzRTRDZjk2Yzk1QjhBMTA0NzAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTcwMTcwODU4NDQ1OCwibmFtZSI6Im5ld2tleSJ9.SZvTcLX9RWvqxdXCn_sQY9Z685fECZTzXSdZHNz5VNw';
+
+const String rpcUrl = //"https://ropsten.infura.io/v3/";
+    "http://127.0.0.1:7545";
+const String wsUrl = // "https://ropsten.infura.io/v3/";
+    "ws://127.0.0.1:7545";
+
+const String contractAddress = "0x4Ac0b5a17878E77466221411e97FDbA046d23FcB";
+
+const int chainId = 1337; //Ropsten-3, Polygon testnet-80001, local ganache-1337
